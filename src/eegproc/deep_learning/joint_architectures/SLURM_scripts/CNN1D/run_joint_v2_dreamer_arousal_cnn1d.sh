441bb6083936056a9992a6553238e22c05352487
VitorInserra <inserra.vitor@gmail.com>
2026-07-20 22:05:08 -0300

    Add kl divergence learning

--- src/eegproc/deep_learning/joint_architectures/SLURM_scripts/CNN1D/run_joint_v2_dreamer_arousal_cnn1d.sh
+++ src/eegproc/deep_learning/joint_architectures/SLURM_scripts/CNN1D/run_joint_v2_dreamer_arousal_cnn1d.sh
@@ -226,9 +226,9 @@ python -m src.eegproc.deep_learning.joint_architectures.joint_v2_autoencoder_vc_
     "ae_loss_weight": [0.3],
     "vc_loss_weight": [1.0],
     "vc_alpha": [1.0],
-    "vc_beta": [0.0],
+    "vc_beta": [0.01],
     "vc_gamma": [0.0],
-    "vc_lambda": [0.0],
+    "vc_lambda": [0.01],
     "vae_beta": [
         0.1
     ],
