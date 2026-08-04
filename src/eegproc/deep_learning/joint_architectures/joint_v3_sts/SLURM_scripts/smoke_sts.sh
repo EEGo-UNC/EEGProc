d2df73878c38663d1ef769d7cade9941397d5813
VitorInserra <inserra.vitor@gmail.com>
2026-08-04 17:25:38 -0400

    Reverse focal alpha

--- src/eegproc/deep_learning/joint_architectures/joint_v3_sts/SLURM_scripts/smoke_sts.sh
+++ src/eegproc/deep_learning/joint_architectures/joint_v3_sts/SLURM_scripts/smoke_sts.sh
@@ -346,7 +346,7 @@ python -m src.eegproc.deep_learning.joint_architectures.joint_v3_sts.joint_sts_m
             0.3
         ],
 
-        "focal_alpha": [0.55, 0.45],
+        "focal_alpha": [0.45, 0.55],
         "focal_gamma": [1.5],
         "vc_alpha": [
             1.0
