6323b1b555d782723cda45feda2a7759256ed7e7
VitorInserra <inserra.vitor@gmail.com>
2026-08-26 13:38:57 -0400

    Revert "Reduce trials"
    This reverts commit b4a7a98ccb641d6a14cefe5efc3fb33df139e4a8.

--- src/eegproc/deep_learning/joint_architectures/SICModelv11/SLURM_scripts/smoke_test_sic_mldg_brier_ablations.sh
+++ src/eegproc/deep_learning/joint_architectures/SICModelv11/SLURM_scripts/smoke_test_sic_mldg_brier_ablations.sh
@@ -225,7 +225,7 @@ print(json.dumps({
     # subject-adversarial loss on A; complete VC emotion loss on adapted B.
     "mldg_meta_train_subjects": 8,
     "mldg_meta_test_subjects": 4,
-    "mldg_trials_per_subject": 3,
+    "mldg_trials_per_subject": 4,
     "mldg_steps_per_epoch": mldg_steps_per_epoch,
     "mldg_inner_learning_rate": 1e-4,
     "mldg_meta_test_weight": 1.0,
