bfadcad6f9e15d2493a304f25d37d8d73fffe558
VitorInserra <inserra.vitor@gmail.com>
2026-09-20 12:11:30 -0400

    Run valence with small reconstruction loss

--- src/eegproc/deep_learning/joint_architectures/SICModelv15/SLURM_scripts/full_run_v15_valence.sh
+++ src/eegproc/deep_learning/joint_architectures/SICModelv15/SLURM_scripts/full_run_v15_valence.sh
@@ -191,7 +191,7 @@ print(json.dumps({
     "use_gcn_gru_branch": True,
     "use_bilstm_branch": True,
     "use_decoder": True,
-    "reconstruction_loss_weight": 0.6,
+    "reconstruction_loss_weight": 0.1,
     "decoder_dropout": 0.1,
     "joint_reconstruction_auxiliary_weight": 0.25,
     "joint_reconstruction_initial_alpha": 0.5,
